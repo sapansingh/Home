b8acc63e0aafd27811ed8fab53668a82fe063d9a
sapan singh <sapansinghchouhan@gmail.com>
2024-08-06 10:47:43 +0530

    first commit

---
+++ src/main/java/com/penalty/Home/Controler/home.java
@@ -0,0 +1,30 @@
+package com.penalty.Home.Controler;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class home {
+    
+    @RequestMapping("/")
+    public String requestMethodName() {
+
+
+
+        return "yes this is working fine";
+    }
+    
+
+    @RequestMapping("/test")
+    public String test() {
+
+
+
+        return "yes this is working fine but this is test page";
+    }
+    
+
+}
